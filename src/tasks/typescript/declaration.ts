e33a04bd08e1275206c0e0ff2cb4987f19a82155
Alduino <git@alduino.dev>
2021-08-26 19:13:11 +1000

    Require that tsconfig has include or files

--- src/tasks/typescript/declaration.ts
+++ src/tasks/typescript/declaration.ts
@@ -5,10 +5,19 @@ import execa from "execa";
 import logger from "consola";
 import {Extractor, ExtractorConfig} from "@microsoft/api-extractor";
 import {mkdir} from "fs/promises";
+import readTsconfig from "../../utils/readTsconfig";
+import invariant from "tiny-invariant";
+import warning from "tiny-warning";
 
 export const buildDeclaration = createStaticTask("Build declaration", async ({tsDeclTempOut, watch, cacheDir, opts}) => {
     const userDir = await getUserDirectory();
     const tsconfigPath = relative(userDir, await resolveUserFile("tsconfig.json"));
+
+    // require tsconfig to have `include` or `files` set
+    const tsconfig = await readTsconfig();
+    invariant(tsconfig.include || tsconfig.files, "tsconfig must have `include` or `files` keys, to exclude temporary build files");
+    warning(!tsconfig.include?.some(it => it.startsWith("**/")), "`include` in tsconfig should restrict to a subdirectory of the project, otherwise temporary build files will be matched");
+    warning(!tsconfig.files?.some(it => it.startsWith("**/")), "`files` in tsconfig should restrict to a subdirectory of the project, otherwise temporary build files will be matched");
 
     const args = [
         "--declaration", "--emitDeclarationOnly",
